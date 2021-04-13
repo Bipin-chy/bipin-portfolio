431aa355c7471f13de71644da52bacb06358a032
Bipin Chaudhary <chaudharibipin121@gmail.com>
2021-04-13 15:26:27 +0545

    navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.bundle";
+import "./App.css";
 import Navbar from './components/Navbar';
 
 function App() {
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
 
 function Navbar() {
     return (
         
-             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
-    <a className="navbar-brand">Navbar</a>
+             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container">
+    <a className="navbar-brand">Bipin Chaudhary</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+      <FontAwesomeIcon icon={faBars}/>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
@@ -16,7 +19,23 @@ function Navbar() {
           <a className="nav-link active" aria-current="page">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link">About me</a>
+          <a className="nav-link" href="#">About me</a>
+        </li>
+
+        <li className="nav-item">
+          <a className="nav-link" href="#">Services</a>
+        </li>
+
+        <li className="nav-item">
+          <a className="nav-link" href="#">How work</a>
+        </li>
+
+        <li className="nav-item">
+          <a className="nav-link" href="#">Portfolio</a>
+        </li>
+
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contact</a>
         </li>
         
       </ul>
